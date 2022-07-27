c5f7a47e0e09e83a8ea0e8fd8d986e3b79c85da9
Taras-Melnyk1987 <dr.melnuk1987@gmail.com>
2022-07-27 03:21:49 +0300

    add componentDidMount, componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,6 +28,21 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   handleFormContact = ({ name, number }) => {
     const contactToAdd = this.checkName(name);
     const editedNumber = this.numberFormatting(number);
